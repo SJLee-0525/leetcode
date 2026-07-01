e616c160c8bb866ae871e0a0628dc748cb2df87f
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2026-07-01 19:42:20 +0900

    Time: 1 ms (76.02%), Space: 60.5 MB (50.97%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let p = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) nums[p++] = nums[i];
+    };
+    
+    for (let j = p; j < nums.length; j++) nums[j] = 0;
+};
