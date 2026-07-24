f173973bbb0a7e3b9e903e165040f98a6ec16679
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2026-07-24 13:50:52 +0900

    Time: 2 ms (83.5%), Space: 53.9 MB (87.57%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
@@ -3,9 +3,13 @@
  * @return {string}
  */
 var smallestSubsequence = function (s) {
-    // 지금 문자가 더 작고, 스택 맨 위 문자가 나중에 또 등장한다면
     const stack = Array();
-    const used = Array(26).fill(0);
+
+    const used = Array(26).fill(0), last = Array(26).fill(-1);
+
+    for (let l = s.length - 1; l >= 0; l--) {
+        if (last[s[l].charCodeAt() - 97] === -1) last[s[l].charCodeAt() - 97] = l;
+    };
 
     function isAgain(w, i) {
         for (let j = i + 1; j < s.length; j++) {
@@ -19,7 +23,9 @@ var smallestSubsequence = function (s) {
         const ascii = s[c].charCodeAt() - 97;
         if (used[ascii]) continue;
 
-        while (stack.length && stack[stack.length - 1] > s[c] && isAgain(stack[stack.length - 1], c)) used[stack.pop().charCodeAt() - 97]--;
+        while (stack.length && stack[stack.length - 1] > s[c] && last[stack[stack.length - 1].charCodeAt() - 97] > c) {
+            used[stack.pop().charCodeAt() - 97]--;
+        };
 
         if (!used[ascii]) {
             used[ascii]++;
