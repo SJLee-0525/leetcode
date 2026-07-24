979625cf371cb1be9e362e1649abf7b7324511cd
Sungjoon Lee <sungjoon25@yonsei.ac.kr>
2026-07-24 13:44:46 +0900

    Time: 1 ms (93.64%), Space: 54.1 MB (85.19%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function (s) {
+    // 지금 문자가 더 작고, 스택 맨 위 문자가 나중에 또 등장한다면
+    const stack = Array();
+    const used = Array(26).fill(0);
+
+    function isAgain(w, i) {
+        for (let j = i + 1; j < s.length; j++) {
+            if (w === s[j]) return true;
+        };
+
+        return false;
+    };
+
+    for (let c = 0; c < s.length; c++) {
+        const ascii = s[c].charCodeAt() - 97;
+        if (used[ascii]) continue;
+
+        while (stack.length && stack[stack.length - 1] > s[c] && isAgain(stack[stack.length - 1], c)) used[stack.pop().charCodeAt() - 97]--;
+
+        if (!used[ascii]) {
+            used[ascii]++;
+            stack.push(s[c]);
+        };
+    };
+
+    return stack.join('');
+};
